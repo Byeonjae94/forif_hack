ac47b6bcbc5e104971a0c0e08344efa8efe786f7
Byeonjae94 <wogus7086@gmail.com>
2017-12-23 23:30:38 +0900

    Initial Commit Naver Map Api

--- app/src/main/java/com/example/user/jidaegong2/MainActivity.java
+++ app/src/main/java/com/example/user/jidaegong2/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.user.jidaegong2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button btn1 = (Button)findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment1 fragment1 = new Fragment1();
+                fragment1.setArguments(new Bundle());
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                fragmentTransaction.add(R.id.fragmentHere, fragment1);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
